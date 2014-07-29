b8c84aa1a3250760a5db276598b5c07754aaabe4
berrigan <o.b@optusnet.com.au>
2014-07-30 01:46:24 +1000

    jquery velocity, some more CSS, LESS compilation setup w/ PhpStorm

--- Gruntfile.js
+++ Gruntfile.js
@@ -23,6 +23,6 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-less');
 
-    grunt.registerTask('default', ['watch']);
+    grunt.registerTask('default', ['less']);
 
 };
